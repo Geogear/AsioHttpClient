72e490cc268716295939b9c949fe281c6bc55a38
unknown <mustafaenesbilgic@hotmail.com>
2021-12-16 13:09:46 +0300

    Add get api-key from file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <asio.hpp>
 
@@ -13,7 +14,26 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    /* Needed paths and names. */
     std::string server = "api.giphy.com";
     std::string path = "/v1/stickers/search";
     std::string api_key_path = argv[1];
+
+    /* Open and file and check if it exists. */
+    std::ifstream my_file;
+    my_file.open(api_key_path);
+    if (!my_file.is_open())
+    {
+        std::cout << "File doesn't exist.\n";
+        return 1;
+    }
+
+    /* Read the api_key. */
+    std::string api_key;
+    my_file >> api_key;
+
+    /* Close the file, don't need it anymore. */
+    my_file.close();
+
+    
 }
